7c446a69a04974fe76e120264937db00216d0efb
Miljan Puletic <miljan.puletic@gmail.com>
2022-05-04 21:13:52 +0200

    strip added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HomeComponent } from './components/home/home.component';
 import { CourtComponent } from './components/court/court.component';
 import { DatePipe } from './utils/date-pipe';
 import { TestComponent } from './components/test/test.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './utils/material-module';
+import { NgxStripeModule } from 'ngx-stripe';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,13 +25,28 @@ import { TestComponent } from './components/test/test.component';
     TestComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    NgbModule,
-    FormsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    FormsModule,
+    NgxStripeModule.forRoot('pk_test_51KrplgL8U3BNM2iQOPu9Y2SCV5j07e8d1oWbwrYwTe3wYc3QkFX1JlveVgBO0AB5ClKnsOxtHcZP3Re7lsE9nDIe00eP1g0G8G'),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+/*
+
+  BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    FormsModule,
+    NgxStripeModule.forRoot('pk_test_51KrplgL8U3BNM2iQOPu9Y2SCV5j07e8d1oWbwrYwTe3wYc3QkFX1JlveVgBO0AB5ClKnsOxtHcZP3Re7lsE9nDIe00eP1g0G8G'),
+    ReactiveFormsModule
+    */
--- src/app/components/court/court.component.ts
+++ src/app/components/court/court.component.ts
@@ -1,7 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Builder } from 'builder-pattern';
 import { Timeslot } from 'src/app/model/timeslot';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { StripeService } from 'ngx-stripe';
+import {
+  StripeElementsOptions,
+  PaymentRequestPaymentMethodEvent,
+  PaymentIntent,
+  PaymentRequestShippingAddressEvent,
+} from '@stripe/stripe-js';
+import { DOCUMENT } from '@angular/common';
+import {loadStripe} from '@stripe/stripe-js';
+import { ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {  StripePaymentElementComponent } from 'ngx-stripe';
+
 
 @Component({
   selector: 'app-court',
@@ -9,12 +24,39 @@ import { Timeslot } from 'src/app/model/timeslot';
   styleUrls: ['./court.component.css']
 })
 export class CourtComponent implements OnInit {
+
+   // public stripe = new Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+  public formDis = true; 
+  public items = [{ amount: 100 }];
+  public stripe = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+  //private document: Document;
+ 
+  @ViewChild(StripePaymentElementComponent)
+  paymentElement: StripePaymentElementComponent;
+ 
+  public stripeForm : FormGroup;
+ 
+  public elementsOptions: StripeElementsOptions = {
+    locale: 'en'
+  };
+ 
+  public paying = false;
+  public loggedInUser = "Pera Peric";
+  public loggedInUserEmail = "pera@gmail.com";
+  public amount = 12;
+
+
+  ///////////////////////////////////
   //paymentForm: FormGroup;
   paymentForm : FormControl;
   public startDate: Date;
   public endDate: Date;
   public timeslots: Timeslot[] = [Builder(Timeslot).start("1650914076517").end("1650914076517").build()];
-  constructor(public fb: FormBuilder) { }
+
+  constructor(private fb: FormBuilder, @Inject(DOCUMENT) private document: Document, private http: HttpClient, private stripeService: StripeService) { 
+
+
+  }
 
   model = {
     left: true,
@@ -28,6 +70,61 @@ export class CourtComponent implements OnInit {
     //   lozinka:       ['CASH' ]p
     // })
     this.paymentForm = new FormControl();
+    this.stripeForm = this.fb.group({
+      //new FormControl({value: 'Nancy', disabled: true}, Validators.required)
+      //name: new FormControl({value: this.loggedInUser, disabled: true}, [Validators.required, Validators.minLength(3)]),
+      name: [this.loggedInUser, [Validators.required, Validators.minLength(3)]],
+      amount: new FormControl({value: this.amount, disabled: true}, [Validators.required, Validators.min(0)]),
+      //amount: [this.amount, [Validators.required, Validators.min(0)]],
+      email: [this.loggedInUserEmail, [Validators.email]],
+    });
+    console.log(this.stripeForm.get('amount')!.value)
+    this.createPaymentIntent(this.stripeForm.get('amount')!.value)
+    .subscribe(clientSecretResponse => {
+      console.log("aadff: " + clientSecretResponse)
+      console.log("aaaaaaaaaaaaa: " + clientSecretResponse.clientSecret)
+      this.elementsOptions.clientSecret = clientSecretResponse.clientSecret;
+    
+    });
+  }
+
+  private createPaymentIntent(amount: number): Observable<any> {
+    return this.http.post<any>(
+      `http://localhost:8080/create-payment-intent`,
+      { amount : amount }
+    );
+  }
+
+  public pay(){
+    if (this.stripeForm.valid) {
+      this.paying = true;
+      this.stripeService.confirmPayment({
+        elements: this.paymentElement.elements,
+        confirmParams: {
+          payment_method_data: {
+            billing_details: {
+              name: this.stripeForm.get('name')!.value
+            }
+          }
+        },
+        redirect: 'if_required'
+      }).subscribe(result => {
+        this.paying = false;
+        console.log('Result', result);
+        if (result.error) {
+          // Show error to your customer (e.g., insufficient funds)
+          alert({ success: false, error: result.error.message });
+        } else {
+          // The payment has been processed!
+          if (result.paymentIntent!.status === 'succeeded') {
+            // Show a success message to your customer
+            alert({ success: true });
+          }
+        }
+      });
+    } else {
+      console.log(this.stripeForm);
+    }
   }
 
   public removeTimeslote(index : number){
