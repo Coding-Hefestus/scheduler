5c0aad4b09332df6489fa80b4214cc89e86a3e42
Miljan Puletic <miljan.puletic@gmail.com>
2022-05-05 21:44:18 +0200

    stripe completed

--- src/app/components/court/court.component.ts
+++ src/app/components/court/court.component.ts
@@ -25,59 +25,49 @@ export class CourtComponent implements OnInit {
   public paymentElement: StripePaymentElementComponent;
   public stripeForm : FormGroup;
   public paymentTypeForm: FormGroup;
-  public elementsOptions: StripeElementsOptions = {
-    locale: 'en'
-  };
+  public elementsOptions: StripeElementsOptions = {locale: 'en'};
  
   //user's data
   public loggedInUser = "Pera Peric";
   public loggedInUserEmail = "pera@gmail.com";
 
   //dummy amount for pricing for single Reservation; every other Reservation added is incremented by 10;
-  public amount = 5;
+  public amount = 0;
 
   //helper variables
   public paymentType: string;
   public startDate: Date;
   public endDate: Date;
+  public paymentIntentExists: boolean;
 
-  //helper timeslots
-  public timeslots: Timeslot[] = [Builder(Timeslot).start("1650914076517").end("1650914076517").build()];
+  //timeslots repository
+  public timeslots: Timeslot[] = []; //[Builder(Timeslot).start("1650914076517").end("1650914076517").build()];
 
   constructor(private fb: FormBuilder, @Inject(DOCUMENT) private document: Document, private http: HttpClient, private stripeService: StripeService) { 
 
 
   }
 
-
-
   ngOnInit(): void {
     //init Stripe
     loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
-    //initialize paymentTypeForm
+    //initialize paymentTypeForm [cash or card]
     this.paymentTypeForm = this.fb.group({
       'type': 1
     });
     
     //default payment type
-    this.paymentType = 'card';
-
+    this.paymentType = 'cash';
 
     //initialize form for user's data
     this.stripeForm = this.fb.group({
       name: [this.loggedInUser, [Validators.required, Validators.minLength(3)]],
-      amount: new FormControl({value: this.amount, disabled: true}, [Validators.required, Validators.min(0)]),
+      amount: new FormControl({value: this.amount, disabled: true}, [Validators.required, Validators.min(5)]),
       email: [this.loggedInUserEmail, [Validators.email, Validators.required]],
     });
    
-    //to be moved once user hits 'Pay' and if 'Card' is selected
-    this.createPaymentIntent(this.stripeForm.get('amount')!.value)
-    .subscribe(clientSecretResponse => {
-      console.log("aaaaaaaaaaaaa: " + clientSecretResponse.clientSecret)
-      this.elementsOptions.clientSecret = clientSecretResponse.clientSecret;
     
-    });
   }
 
   changePaymentTypeToCash(){
@@ -93,6 +83,64 @@ export class CourtComponent implements OnInit {
       `http://localhost:8080/create-payment-intent`,
       { amount : amount }
     );
+  }
+
+  public removeTimeslote(index : number){
+    this.resetPaymentIntent()
+    if (this.timeslots.length == 0) return;
+    this.timeslots.splice(index, 1);
+    //this.stripeForm.controls['amount'].value
+    //this.stripeForm.controls['amount'].setValue(5)
+    if (this.timeslots.length == 1) this.stripeForm.controls['amount'].setValue(5)
+    //this.amount = 5
+    else if (this.timeslots.length > 1) this.stripeForm.controls['amount'].setValue(this.stripeForm.controls['amount'].value - 10)
+    //this.amount = this.amount - 10;
+    else if (this.timeslots.length == 0) this.stripeForm.controls['amount'].setValue(0)
+    //this.amount = 0;
+  }
+
+  public addTimeslot(){
+    if (this.startDate == null || this.startDate== undefined || this.endDate == null || this.endDate == undefined){
+      alert("Start and end date must be valid for a given timeslot!");
+      return;
+    }  
+    this.resetPaymentIntent()
+
+    this.timeslots.push(Builder(Timeslot).start(this.startDate.getTime().toString()).end(this.endDate.getTime().toString()).build())
+    if (this.timeslots.length == 1) this.stripeForm.controls['amount'].setValue(5);
+    else if (this.timeslots.length > 1) this.stripeForm.controls['amount'].setValue(this.stripeForm.controls['amount'].value + 10);
+
+  }
+  
+  public resetPaymentIntent(){
+    this.elementsOptions =  {locale: 'en'};
+    this.paymentIntentExists = false;
+  }
+
+  public fetchPaymentIntent(){
+    //create payment intent if there is at least single reservation..
+
+    //console.log(this.stripeForm.get('amount')!.value)
+    this.createPaymentIntent(this.stripeForm.get('amount')!.value)
+    .subscribe(clientSecretResponse => {
+      console.log("payment intent: " + clientSecretResponse.clientSecret)
+      this.elementsOptions.clientSecret = clientSecretResponse.clientSecret;
+      this.paymentIntentExists = true;
+    });
+  }
+
+  public payByCash(){
+    console.log("paying by cash...")
+  }
+
+  public dateChangedStart(eventDate: string): Date | null {
+    this.startDate = new Date(eventDate.toString())
+    return !!eventDate ? new Date(eventDate) : null;
+  }
+
+  public dateChangedEnd(eventDate: string): Date | null {
+    this.endDate = new Date(eventDate.toString())
+    return !!eventDate ? new Date(eventDate) : null;
   }
 
   public pay(){
@@ -126,29 +174,6 @@ export class CourtComponent implements OnInit {
     }
   }
 
-  public removeTimeslote(index : number){
-    if (this.timeslots.length == 0) return;
-    this.timeslots.splice(index, 1);
-    if (this.timeslots.length == 1) this.amount = 5
-    else if (this.timeslots.length > 1) this.amount = this.amount - 10;
-    else if (this.timeslots.length == 0) this.amount = 0;
-  }
-
-  public addTimeslot(){
-    this.timeslots.push(Builder(Timeslot).start(this.startDate.getTime().toString()).end(this.endDate.getTime().toString()).build())
-    if (this.timeslots.length == 1) this.amount = 5
-    else if (this.timeslots.length > 1) this.amount = this.amount + 10;
-
-  }
-
-  public dateChangedStart(eventDate: string): Date | null {
-    this.startDate = new Date(eventDate.toString())
-    return !!eventDate ? new Date(eventDate) : null;
-  }
-
-  public dateChangedEnd(eventDate: string): Date | null {
-    this.endDate = new Date(eventDate.toString())
-    return !!eventDate ? new Date(eventDate) : null;
-  }
+  
 
 }
