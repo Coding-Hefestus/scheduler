58b4611a0480ac129780cfbfcd94f860a97531f9
Miljan Puletic <miljan.puletic@gmail.com>
2022-05-05 14:24:30 +0200

    organize files

--- src/app/components/court/court.component.ts
+++ src/app/components/court/court.component.ts
@@ -5,12 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { StripeService } from 'ngx-stripe';
-import {
-  StripeElementsOptions,
-  PaymentRequestPaymentMethodEvent,
-  PaymentIntent,
-  PaymentRequestShippingAddressEvent,
-} from '@stripe/stripe-js';
+import {StripeElementsOptions,PaymentRequestPaymentMethodEvent,PaymentIntent,PaymentRequestShippingAddressEvent} from '@stripe/stripe-js';
 import { DOCUMENT } from '@angular/common';
 import {loadStripe} from '@stripe/stripe-js';
 import { ViewChild } from '@angular/core';
@@ -25,32 +20,27 @@ import {  StripePaymentElementComponent } from 'ngx-stripe';
 })
 export class CourtComponent implements OnInit {
 
-   // public stripe = new Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
-  public formDis = true; 
-  public items = [{ amount: 100 }];
-  public stripe = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
-  //private document: Document;
- 
+  //stripe elements
   @ViewChild(StripePaymentElementComponent)
-  paymentElement: StripePaymentElementComponent;
- 
+  public paymentElement: StripePaymentElementComponent;
   public stripeForm : FormGroup;
- 
+  public paymentTypeForm: FormGroup;
   public elementsOptions: StripeElementsOptions = {
     locale: 'en'
   };
  
-  public paying = false;
+  //user's data
   public loggedInUser = "Pera Peric";
   public loggedInUserEmail = "pera@gmail.com";
+
+  //dummy amount for pricing
   public amount = 12;
 
-
-  ///////////////////////////////////
-  //paymentForm: FormGroup;
-  paymentForm : FormControl;
+  //helper variables
   public startDate: Date;
   public endDate: Date;
+
+  //helper timeslots
   public timeslots: Timeslot[] = [Builder(Timeslot).start("1650914076517").end("1650914076517").build()];
 
   constructor(private fb: FormBuilder, @Inject(DOCUMENT) private document: Document, private http: HttpClient, private stripeService: StripeService) { 
@@ -58,34 +48,39 @@ export class CourtComponent implements OnInit {
 
   }
 
-  model = {
-    left: true,
-    middle: false,
-    right: false
-  };
+
 
   ngOnInit(): void {
-    // this.paymentForm = this.fb.group({
-    //   korisnickoIme: ['CARD'],
-    //   lozinka:       ['CASH' ]p
-    // })
-    this.paymentForm = new FormControl();
+    //init Stripe
+    loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+    //initialize paymentTypeForm
+    this.paymentTypeForm = this.fb.group({
+      'type': 1
+    });
+
+    //initialize form for user's data
     this.stripeForm = this.fb.group({
-      //new FormControl({value: 'Nancy', disabled: true}, Validators.required)
-      //name: new FormControl({value: this.loggedInUser, disabled: true}, [Validators.required, Validators.minLength(3)]),
       name: [this.loggedInUser, [Validators.required, Validators.minLength(3)]],
       amount: new FormControl({value: this.amount, disabled: true}, [Validators.required, Validators.min(0)]),
-      //amount: [this.amount, [Validators.required, Validators.min(0)]],
-      email: [this.loggedInUserEmail, [Validators.email]],
+      email: [this.loggedInUserEmail, [Validators.email, Validators.required]],
     });
-    console.log(this.stripeForm.get('amount')!.value)
+   
+    //to be moved once user hits 'Pay' and if 'Card' is selected
     this.createPaymentIntent(this.stripeForm.get('amount')!.value)
     .subscribe(clientSecretResponse => {
-      console.log("aadff: " + clientSecretResponse)
       console.log("aaaaaaaaaaaaa: " + clientSecretResponse.clientSecret)
       this.elementsOptions.clientSecret = clientSecretResponse.clientSecret;
     
     });
+  }
+
+  changePaymentTypeToCash(){
+     console.log("menjam u cash")
+  }
+
+  changePaymentTypeToCard(){
+    console.log("menjam u card")
   }
 
   private createPaymentIntent(amount: number): Observable<any> {
@@ -97,7 +92,6 @@ export class CourtComponent implements OnInit {
 
   public pay(){
     if (this.stripeForm.valid) {
-      this.paying = true;
       this.stripeService.confirmPayment({
         elements: this.paymentElement.elements,
         confirmParams: {
@@ -109,7 +103,6 @@ export class CourtComponent implements OnInit {
         },
         redirect: 'if_required'
       }).subscribe(result => {
-        this.paying = false;
         console.log('Result', result);
         if (result.error) {
           // Show error to your customer (e.g., insufficient funds)
@@ -136,14 +129,11 @@ export class CourtComponent implements OnInit {
   }
 
   public dateChangedStart(eventDate: string): Date | null {
-    //console.log(eventDate)
     this.startDate = new Date(eventDate.toString())
-
     return !!eventDate ? new Date(eventDate) : null;
   }
 
   public dateChangedEnd(eventDate: string): Date | null {
-    //console.log(eventDate)
     this.endDate = new Date(eventDate.toString())
     return !!eventDate ? new Date(eventDate) : null;
   }
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-test',
@@ -7,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   public filterDateFrom: Date;
+  public radioGroupForm: FormGroup;
   date: string;
-    constructor() {
+    constructor(private http: HttpClient, private fb: FormBuilder) {
         this.date = new Date().toISOString().slice(0, 16);
     }
   ngOnInit(): void {
+    // this.createPaymentIntent(23)
+    // .subscribe(clientSecretResponse => {
+    //   console.log("aadff: " + clientSecretResponse)
+    //   console.log("aaaaaaaaaaaaa: " + clientSecretResponse['clientSecret'])
+
+    
+     // });
+     this.radioGroupForm = this.fb.group({
+      'type': 1
+    });
   }
   public myFunction() {
     var x = document.getElementById("myLocalDate");
@@ -24,5 +38,14 @@ export class TestComponent implements OnInit {
     var x = new Date(eventDate.toString())
     console.log(x.getTime())
     return !!eventDate ? new Date(eventDate) : null;
+  }
+
+  
+
+  private createPaymentIntent(amount: number): Observable<number> {
+    return this.http.post<number>(
+      `http://localhost:8080/create-payment-intent`,
+      { amount : amount }
+    );
   }
 }
